6ebebd7a390ea26232bcf8fa3be5bb6216630116
Dave Bajuyo <davebajuyo@apollo.com.ph>
2025-12-23 11:58:18 +0800

    feat: abstracted repository contract

---
+++ lib/domain/repository/NoteEntryRepository.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:sample_notes/core/error/failures.dart';
+import 'package:sample_notes/domain/entities/NoteEntry.dart';
+
+abstract class NoteEntryRepository {
+  Future<Either<Failure, List<NoteEntry>>> getAllNoteEntries();
+  Future<Either<Failure, NoteEntry>> getNoteEntry(String id);
+  Future<Either<Failure, NoteEntry>> createNoteEntry(NoteEntry note);
+  Future<Either<Failure, NoteEntry>> updateNoteEntry(NoteEntry note);
+}
