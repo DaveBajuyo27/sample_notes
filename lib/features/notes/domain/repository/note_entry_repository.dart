e79133e368973aa2d7b0df84ebc1e4761a55dc0f
Dave Bajuyo <davebajuyo@apollo.com.ph>
2025-12-26 11:06:25 +0800

    chore: restructured folders to match clean architecture

--- lib/features/notes/domain/repository/note_entry_repository.dart
+++ lib/features/notes/domain/repository/note_entry_repository.dart
@@ -1,10 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:sample_notes/core/error/failures.dart';
-import 'package:sample_notes/domain/entities/note_entry.dart';
+import 'package:sample_notes/features/notes/domain/entities/note_entry.dart';
 
 abstract class NoteEntryRepository {
   Future<Either<Failure, List<NoteEntry>>> getAllNoteEntries();
   Future<Either<Failure, NoteEntry>> getNoteEntry(String id);
   Future<Either<Failure, NoteEntry>> createNoteEntry(NoteEntry note);
   Future<Either<Failure, NoteEntry>> updateNoteEntry(NoteEntry note);
+  Future<Either<Failure, void>> deleteNoteEntry(String id);
 }
