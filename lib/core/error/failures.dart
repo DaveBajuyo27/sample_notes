d5e77a675361c9ffce562d615237fb759a3e1ea0
Dave Bajuyo <davebajuyo@apollo.com.ph>
2025-12-29 09:38:07 +0800

    feat: added custom exception and failure

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -2,4 +2,9 @@ import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable {
   const Failure();
+
+  @override
+  List<Object?> get props => [];
 }
+
+class LocalStorageFailure extends Failure {}
