942dd9f8114288427fdc18b551b7abb11c1b8580
Dave Bajuyo <davebajuyo@apollo.com.ph>
2025-12-29 09:41:26 +0800

    test: updated auto generated mocks

---
+++ test/features/notes/mocks/note_entry_local_datasource_mocks.dart
@@ -0,0 +1,5 @@
+import 'package:mockito/annotations.dart';
+import 'package:sample_notes/features/notes/data/datasources/note_entry_local_data_source.dart';
+
+@GenerateMocks([NoteEntryLocalDataSource])
+void main() {}
