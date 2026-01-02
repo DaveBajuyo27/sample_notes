f564faf3d4fc736f57864924670492bee1a56be8
Dave Bajuyo <davebajuyo@apollo.com.ph>
2026-01-02 10:28:45 +0800

    feat: added data source implementation

--- lib/features/notes/data/datasources/note_entry_local_data_source.dart
+++ lib/features/notes/data/datasources/note_entry_local_data_source.dart
@@ -1,3 +1,5 @@
+import 'package:hive/hive.dart';
+import 'package:sample_notes/core/error/exceptions.dart';
 import 'package:sample_notes/features/notes/data/models/note_entry_model.dart';
 
 abstract class NoteEntryLocalDataSource {
@@ -7,4 +9,42 @@ abstract class NoteEntryLocalDataSource {
   Future<NoteEntryModel> createNoteEntry(NoteEntryModel note);
   Future<NoteEntryModel> updateNoteEntry(NoteEntryModel note);
   Future<void> deleteNoteEntry(String id);
+}
+
+class NoteEntryLocalDataSourceImpl implements NoteEntryLocalDataSource {
+  final Box<NoteEntryModel> noteBox;
+
+  NoteEntryLocalDataSourceImpl(this.noteBox);
+
+  @override
+  Future<NoteEntryModel> createNoteEntry(NoteEntryModel note) async {
+    await noteBox.put(note.id, note);
+
+    return note;
+  }
+
+  @override
+  Future<void> deleteNoteEntry(String id) async {
+    await getNoteEntry(id);
+    await noteBox.delete(id);
+  }
+
+  @override
+  Future<List<NoteEntryModel>> getAllNoteEntries() async {
+    return noteBox.values.toList();
+  }
+
+  @override
+  Future<NoteEntryModel> getNoteEntry(String id) async {
+    final result = noteBox.get(id);
+    if (result == null) throw NoteNotFoundException();
+    return result;
+  }
+
+  @override
+  Future<NoteEntryModel> updateNoteEntry(NoteEntryModel note) async {
+    await getNoteEntry(note.id);
+    await noteBox.put(note.id, note);
+    return note;
+  }
 }
